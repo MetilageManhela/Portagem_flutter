23b0876bb99b2b3512c00af8dfff67bfe7ac4902
Metilage <metilahilario@gmail.com>
2022-09-06 10:27:48 +0200

    Upload

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:portagem_web/screens/login.dart';
+
+
+
+void main() => runApp(const MyApp()); 
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+@override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+    
+      debugShowCheckedModeBanner: false,
+      //home:   const MainMenu(),
+      home: const LoginPage(),
+      builder: EasyLoading.init(),
+    );
+  }
+}
